ad9a259efa7d50d52fa5e18bfc64c9dfaf3c0a94
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-02-21 01:06:55 +0300

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,30 +1,30 @@
-const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
-const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
 
-const $ = selector => document.querySelectorAll(selector)
+const $ = selector => document.querySelectorAll(selector);
 
-const keys = $('.key')
-const keysBlack = $('.key.black')
-const keysWhite = $('.key.white')
+const keys = $('.key');
+const keysBlack = $('.key.black');
+const keysWhite = $('.key.white');
 
 keys.forEach(key => {
-    key.addEventListener('click', () => playNote(key))
+    key.addEventListener('click', () => playNote(key));
 })
 
 document.addEventListener('keydown', e => {
-    if (e.repeat) return
-    const keysBlackIndex = BLACK_KEYS.indexOf(e.key)
-    const keysWhiteIndex = WHITE_KEYS.indexOf(e.key)
-    if (keysBlackIndex > -1) playNote(keysBlack[keysBlackIndex])
-    if (keysWhiteIndex > -1) playNote(keysWhite[keysWhiteIndex])
+    if (e.repeat) return true;
+    const keysBlackIndex = BLACK_KEYS.indexOf(e.key);
+    const keysWhiteIndex = WHITE_KEYS.indexOf(e.key);
+    if (keysBlackIndex > -1) playNote(keysBlack[keysBlackIndex]);
+    if (keysWhiteIndex > -1) playNote(keysWhite[keysWhiteIndex]);
 })
 
 function playNote(key) {
-    const sound = document.getElementById(key.dataset.note)
-    sound.currentTime = 0
-    sound.play()
-    key.classList.add('active')
+    const sound = document.getElementById(key.dataset.note);
+    sound.currentTime = 0;
+    sound.play();
+    key.classList.add('active');
     sound.addEventListener('ended', () => {
-        key.classList.remove('active')
+        key.classList.remove('active');
     })
 }
